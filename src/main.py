a932c4b8f573beb32536892cbff6290088039aff
Elias Gill <eliasgill42@gmail.com>
2025-04-05 14:06:25 -0300

    feat: poetry for dependency management

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 """
 Created on 2025-04-05 12:30:36
 
-@authors: 
+@authors:
     - Elias Sebastian Gill Quintana
     - Maria Jose Mendoza Recalde
     - Abigail Mercedes Nuñes Mendez
@@ -11,31 +11,12 @@ Created on 2025-04-05 12:30:36
 
 import cv2
 
-# Utilizando el comando imread de opencv, leeremos la imagen 1.jpeg y la guardaremos en la variable img
-def show_img(img: str):
-    img = cv2.imread(img)
+
+def show_img(path: str):
+    img = cv2.imread(path)
     if img is None:
         print("Error: No se pudo cargar la imagen")
     else:
-        cv2.imshow("ventana1", img)
+        cv2.imshow(path, img)
         cv2.waitKey()
         cv2.destroyAllWindows()
-
-img = cv2.imread("1.jpeg")
-img2 = cv2.imread("1.jpeg",0)
-
-cv2.imshow("ventana", img) ## Mostramos la imagen en la ventana "ventana", es redimensionable pero no cambia su escala
-
-
-while True:
-    
-    key = cv2.waitKey()
-    
-    if key == ord("4"):
-        cv2.imshow("ventana", img)
-    elif key == ord("6"):
-        cv2.imshow("ventana", img2)
-    else:
-        break
-    
-cv2.destroyAllWindows()
